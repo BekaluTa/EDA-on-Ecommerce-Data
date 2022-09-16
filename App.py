178ed1c9e25e33a9647c1202631b212193165493
BekaluTa <bekiman21@gmail.com>
2022-09-16 15:36:44 +0300

    Dasboarding Using Streamlit

---
+++ App.py
@@ -0,0 +1,33 @@
+
+import streamlit as st
+from PIL import Image
+from streamlit_option_menu import option_menu
+
+from Pages.home import home_app
+from Pages.overview import overview_app
+from Pages.predict import predict_app
+
+st.set_page_config(page_title="Sales Prediction on Pharmacy Data")
+
+slected = option_menu(
+    menu_title=None,
+    options=["Home", "Overview", "Predict"],
+    icons=["house", "globe2", "app"],
+    menu_icon="cast",
+    orientation="horizontal"
+)
+styles = {
+    "container": {"padding": "0!important", "background-color": "#fafafa"},
+    "icon": {"color": "orange", "font-size": "25px"},
+    "nav-link": {"font-size": "25px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
+    "nav-link-selected": {"background-color": "green"},
+}
+st.title("Sales Data Prediction")
+
+
+if slected == "Home":
+    home_app()
+elif slected == "Overview":
+    overview_app()
+else:
+    prdict_app()
